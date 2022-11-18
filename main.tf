129596c51b9676b266ff4296eb46e0e664b350ca
Joseph <josiokoko@gmail.com>
2022-11-18 00:52:04 -0700

    Updated Jenkinsfile

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "web" {
+  count         = 3
   ami           = "ami-0b0dcb5067f052a63"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "exxample"
+    Name = "joseph-example-${terraform.workspacer}-${count.index}"
   }
 }
